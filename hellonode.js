a3950023c73b7484c68175ff383edb3a2c04c10f
HZP <wenfangzheng@hzp-MacBook-Air.local>
2016-05-18 00:33:01 +0800

    hello

--- hellonode.js
+++ hellonode.js
@@ -1,43 +1,8 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var Wilddog = require("wilddog");
-var ref = new Wilddog("https://jiejiegao.wilddogio.com/");
-// 创建 application/x-www-form-urlencoded 编码解析
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var http=require('http');
 
-app.use(express.static('public'));
+http.createServer(function(req,res){
+  res.writeHead(200,{'Content-type':'text/plain'});
+  res.end('hello');
+}).listen(3000);
 
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
-})
-
-app.post('/register', urlencodedParser, function (req, res) {
-
-   ref.createUser({email:"Loki@asgard.com",password:"examplepassword"},
-    function(err,data){
-    if(err!=null){
-      //not success
-     错误
-    } else {
-      //create user success
-      成功
-    }
-  });
-   // 输出 JSON 格式
-   response = {
-       Name:req.body.Name,
-       Pass:req.body.Pass
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
-})
+console.log('hahah');
